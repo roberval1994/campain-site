532fe86e12a8f0a61215a1bbe2fec2bd774f951a
Roberval <roberval1994@live.com>
2020-07-16 16:51:07 -0300

    Site no Ar

--- campaign/plataform/urls.py
+++ campaign/plataform/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.Index, name='Index')
+    path('', views.Index, name='Index'),
+    path('Index.html', views.Index, name='Index'),
+    path('about_me.html', views.about_me, name='about_me'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- campaign/plataform/views.py
+++ campaign/plataform/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Index(request):
     return render(request, 'Index.html', {})
+
+def about_me(request):
+    return render(request, 'about_me.html', {})
